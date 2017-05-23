a0db1b0a2b3cfc8afe94837a0e517368d2c0e3bb
dhvanan <dhvanan@gmail.com>
2017-05-23 14:41:30 -0700

    Added Oauth user verification

--- git_sync/flask_client.py
+++ git_sync/flask_client.py
@@ -12,7 +12,6 @@ dict_to_send = {'command': 'push'}
 # User credentials format - 
 # dict_to_send['user_credentials'] = {'username':'<Username>', 'oauth_token': '<Oauth Token>'}
 dict_to_send['user_credentials'] = {'username':'Dhvanan', 'oauth_token': '20d2da0882252c35d4a758779152daef3544d651'}
-# dict_to_send['user_credentials'] = {'username':'dhvananshah', 'oauth_token': '814804fc99eb0ae517b785d293486b07b8802a7b'}
 
 if dict_to_send['command'] == 'pull':
     res = requests.post('http://localhost:5000/git-sync', json=dict_to_send)
